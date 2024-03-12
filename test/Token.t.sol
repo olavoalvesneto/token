6912eb820ea765ff8f69a7ad9e8b304ba473fb70
Olavo Alves <olavoalvesneto@hotmail.com>
2024-03-12 20:26:54 -0300

    [TOKEN-1] Added lic and adj the import

--- test/Token.t.sol
+++ test/Token.t.sol
@@ -1,14 +1,14 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Token} from "../src/Counter.sol";
+import {Token} from "../src/Token.sol";
 
 contract CounterTest is Test {
     Token public token;
 
     function setUp() public {
-        token = new Counter();
+        token = new Token();
         token.setNumber(0);
     }
 
