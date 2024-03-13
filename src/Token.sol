3fdaa542baa977b63aef57c20de856848cbd329d
Olavo Alves <olavoalvesneto@hotmail.com>
2024-03-12 23:16:42 -0300

    [TOKEN-1] Implemented the contract

--- src/Token.sol
+++ src/Token.sol
@@ -2,4 +2,48 @@
 pragma solidity ^0.8.13;
 
 contract Token {
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+
+    event Transfer(address from, address to, uint256 amount);
+    event Approval(address _owner, address _spender, uint256 _value);
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowances;
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        uint256 balanceOfUser = balances[_owner];
+        return balanceOfUser;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value, "INSUFFICIENT_AMOUNT");
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+
+        //O QUE E CHAMADA DO TIPO SOCKET
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public {
+        require(allowances[_from][msg.sender] >= _value, "INSUFFICIENT_ALLOWANCE");
+
+        balances[_from] -= _value;
+        balances[_to] += _value;
+
+        emit Transfer(_from, _to, _value);
+
+    }
+
+    function approve(address _spender, uint256 _value) public {
+        allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowances[_owner][_spender];
+    }
 }
--- test/Token.t.sol
+++ test/Token.t.sol
@@ -5,20 +5,5 @@ import {Test, console} from "forge-std/Test.sol";
 import {Token} from "../src/Token.sol";
 
 contract CounterTest is Test {
-    Token public token;
 
-    function setUp() public {
-        token = new Token();
-        token.setNumber(0);
-    }
-
-    function test_Increment() public {
-        token.increment();
-        assertEq(token.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        token.setNumber(x);
-        assertEq(token.number(), x);
-    }
 }
